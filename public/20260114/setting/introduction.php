db8b8e8d9aa7d9cc528ecfd9db5294c012f82975
yuuuuuuuuuuumi <131945896+yuuuuuuuuuuumi@users.noreply.github.com>
2026-01-28 10:16:19 +0900

    1/28 commit

--- public/20260114/setting/introduction.php
+++ public/20260114/setting/introduction.php
@@ -5,23 +5,18 @@ if (empty($_SESSION['login_user_id'])) {
   header("Location: /login.php");
   return;
 }
-// DBに接続
 $dbh = new PDO('mysql:host=mysql;dbname=example_db', 'root', '');
-// セッションにあるログインIDから、ログインしている対象の会員情報を引く
 $select_sth = $dbh->prepare("SELECT * FROM users WHERE id = :id");
 $select_sth->execute([
     ':id' => $_SESSION['login_user_id'],
 ]);
 $user = $select_sth->fetch();
 if (isset($_POST['introduction'])) {
-  // フォームから introduction が送信されてきた場合の処理
-  // ログインしている会員情報のintroductionカラムを更新する
   $update_sth = $dbh->prepare("UPDATE users SET introduction = :introduction WHERE id = :id");
   $update_sth->execute([
       ':id' => $user['id'],
       ':introduction' => $_POST['introduction'],
   ]);
-  // 成功したら成功したことを示すクエリパラメータつきのURLにリダイレクト
   header("HTTP/1.1 302 Found");
   header("Location: ./introduction.php?success=1");
   return;
