b88a514c8c58ee8e63540282326a0537a9c5d71e
yuuuuuuuuuuumi <131945896+yuuuuuuuuuuumi@users.noreply.github.com>
2026-01-28 09:58:40 +0900

    1/28 commit

--- public/20260114/follow_list.php
+++ public/20260114/follow_list.php
@@ -1,18 +1,15 @@
 <?php
 session_start();
 
-// ログインしてなければログイン画面に飛ばす
 if (empty($_SESSION['login_user_id'])) {
   header("HTTP/1.1 302 Found");
   header("Location: /login.php");
   return;
 }
 
-// DBに接続
 $dbh = new PDO('mysql:host=mysql;dbname=example_db', 'root', '');
 
-// 自分がフォローしている一覧をDBから引く。
-// テーブル結合を使って、フォローしている対象の会員情報も一緒に取得。
+// フォローしている一覧
 $select_sth = $dbh->prepare(
   'SELECT user_relationships.*, users.name AS followee_user_name, users.icon_filename AS followee_user_icon_filename'
   . ' FROM user_relationships INNER JOIN users ON user_relationships.followee_user_id = users.id'
@@ -31,7 +28,7 @@ include_once('header.php');
   <?php foreach($select_sth as $relationship): ?>
   <li>
     <a href="i./profile.php?user_id=<?= $relationship['followee_user_id'] ?>">
-      <?php if(!empty($relationship['followee_user_icon_filename'])): // アイコン画像がある場合は表示 ?>
+      <?php if(!empty($relationship['followee_user_icon_filename'])): ?>
       <img src="/image/<?= $relationship['followee_user_icon_filename'] ?>"
         style="height: 2em; width: 2em; border-radius: 50%; object-fit: cover;">
       <?php endif; ?>
