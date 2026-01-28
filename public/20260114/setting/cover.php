8503f8807387c603193cf4593de0e9d08067b924
yuuuuuuuuuuumi <131945896+yuuuuuuuuuuumi@users.noreply.github.com>
2026-01-28 10:10:53 +0900

    1/28 commit

--- public/20260114/setting/cover.php
+++ public/20260114/setting/cover.php
@@ -7,9 +7,7 @@ if (empty($_SESSION['login_user_id'])) {
   return;
 }
 
-// DBに接続
 $dbh = new PDO('mysql:host=mysql;dbname=example_db', 'root', '');
-// セッションにあるログインIDから、ログインしている対象の会員情報を引く
 $select_sth = $dbh->prepare("SELECT * FROM users WHERE id = :id");
 $select_sth->execute([
     ':id' => $_SESSION['login_user_id'],
@@ -17,31 +15,24 @@ $select_sth->execute([
 $user = $select_sth->fetch();
 
 if (isset($_POST['image_base64'])) {
-  // POSTで送られてくるフォームパラメータ image_base64 がある場合
 
   $image_filename = null;
   if (!empty($_POST['image_base64'])) {
-    // 先頭の data:~base64, のところは削る
     $base64 = preg_replace('/^data:.+base64,/', '', $_POST['image_base64']);
 
-    // base64からバイナリにデコードする
     $image_binary = base64_decode($base64);
 
-    // 新しいファイル名を決めてバイナリを出力する
     $image_filename = strval(time()) . bin2hex(random_bytes(25)) . '.png';
     $filepath =  '/var/www/upload/image/' . $image_filename;
     file_put_contents($filepath, $image_binary);
   }
 
-  // ログインしている会員情報のカバー画像ファイル名を更新する
   $update_sth = $dbh->prepare("UPDATE users SET cover_filename = :cover_filename WHERE id = :id");
   $update_sth->execute([
       ':id' => $user['id'],
       ':cover_filename' => $image_filename,
   ]);
 
-  // 処理が終わったらリダイレクトする
-  // リダイレクトしないと，リロード時にまた同じ内容でPOSTすることになる
   header("HTTP/1.1 302 Found");
   header("Location: ./cover.php");
   return;
@@ -62,13 +53,12 @@ include_once('../header.php');
   <?php endif; ?>
 </div>
 
-<!-- フォームのPOST先はこのファイル自身にする -->
 <form method="POST" action="./cover.php">
   <div style="margin: 1em 0;">
     <input type="file" accept="image/*" name="image" id="imageInput">
   </div>
-  <input id="imageBase64Input" type="hidden" name="image_base64"><!-- base64を送る用のinput (非表示) -->
-  <canvas id="imageCanvas" style="display: none;"></canvas><!-- 画像縮小に使うcanvas (非表示) -->
+  <input id="imageBase64Input" type="hidden" name="image_base64">
+  <canvas id="imageCanvas" style="display: none;"></canvas>
   <button type="submit">アップロード</button>
 </form>
 
@@ -79,43 +69,39 @@ document.addEventListener("DOMContentLoaded", () => {
   const imageInput = document.getElementById("imageInput");
   imageInput.addEventListener("change", () => {
     if (imageInput.files.length < 1) {
-      // 未選択の場合
       return;
     }
 
     const file = imageInput.files[0];
-    if (!file.type.startsWith('image/')){ // 画像でなければスキップ
+    if (!file.type.startsWith('image/')){ 
       return;
     }
 
     // 画像縮小処理
-    const imageBase64Input = document.getElementById("imageBase64Input"); // base64を送るようのinput
-    const canvas = document.getElementById("imageCanvas"); // 描画するcanvas
+    const imageBase64Input = document.getElementById("imageBase64Input"); 
+    const canvas = document.getElementById("imageCanvas"); 
     const reader = new FileReader();
     const image = new Image();
-    reader.onload = () => { // ファイルの読み込み完了したら動く処理を指定
-      image.onload = () => { // 画像として読み込み完了したら動く処理を指定
+    reader.onload = () => { // ファイルの読み込み完了
+      image.onload = () => { // 画像として読み込み完了
 
-        // 元の縦横比を保ったまま縮小するサイズを決めてcanvasの縦横に指定する
-        const originalWidth = image.naturalWidth; // 元画像の横幅
-        const originalHeight = image.naturalHeight; // 元画像の高さ
-        const maxLength = 1000; // 横幅も高さも1000以下に縮小するものとする
-        if (originalWidth <= maxLength && originalHeight <= maxLength) { // どちらもmaxLength以下の場合そのまま
+        const originalWidth = image.naturalWidth; // 横幅
+        const originalHeight = image.naturalHeight; // 高さ
+        const maxLength = 1000; // 1000以下に縮小
+        if (originalWidth <= maxLength && originalHeight <= maxLength) { 
             canvas.width = originalWidth;
             canvas.height = originalHeight;
-        } else if (originalWidth > originalHeight) { // 横長画像の場合
+        } else if (originalWidth > originalHeight) { 
             canvas.width = maxLength;
             canvas.height = maxLength * originalHeight / originalWidth;
-        } else { // 縦長画像の場合
+        } else {
             canvas.width = maxLength * originalWidth / originalHeight;
             canvas.height = maxLength;
         }
 
-        // canvasに実際に画像を描画 (canvasはdisplay:noneで隠れているためわかりにくいが...)
         const context = canvas.getContext("2d");
         context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-        // canvasの内容をbase64に変換しinputのvalueに設定
         imageBase64Input.value = canvas.toDataURL();
       };
       image.src = reader.result;
